45f5db99459f65b76711049b77fc069998117aaa
naveendoddi <prasadnaveen847@gmail.com>
2024-02-14 17:17:16 +0530

    m=1

---
+++ script.js
@@ -0,0 +1,13 @@
+const toBase64 = obj => {
+    const str = JSON.stringify (obj);
+    console.log(Buffer.from("naveen"))
+    return Buffer.from(str).toString ('base64');
+};
+
+const header = {
+    alg: 'HS256',
+    typ: 'JWT',
+};
+const b64Header = toBase64 (header);
+const jwtB64Header = replaceSpecialChars(b64Header);
+console.log ("the header is: ",jwtB64Header); 
