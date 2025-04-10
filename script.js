657f2224788214d572d365312cd37612917a95bf
naveendoddi <prasadnaveen847@gmail.com>
2025-04-10 21:59:50 +0530

    pushing new skills

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+  const texts = [
+    "MERN Stack Developer",
+    "Full Stack Developer",
+    "Student",
+    "Developer",
+    "Fresher"
+  ];
+
+  const typingSpan = document.querySelector('.typing-text');
+  let index = 0;
+
+  function typeNextText() {
+    typingSpan.style.animation = 'none'; // Reset typing animation
+    void typingSpan.offsetWidth; // Trigger reflow
+    typingSpan.textContent = texts[index];
+    if(index < 2){
+
+    
+    typingSpan.style.animation = 'typing 4s steps(25, end), blink 0.7s step-end infinite alternate';
+    }else{
+      typingSpan.style.animation = 'typing 2s steps(10, end), blink 0.4s step-end infinite alternate';  
+    }
+    index = (index + 1) % texts.length;
+    setTimeout(typeNextText, 4000); // Match with typing animation duration
+  }
+
+  typeNextText();
+
+
 // Function to handle intersection observations
 function handleIntersection(entries, observer) {
     entries.forEach(entry => {
